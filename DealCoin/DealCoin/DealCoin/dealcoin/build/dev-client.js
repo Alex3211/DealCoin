22ec396d74b7494175524996a99d0fdf91623432
Antoine Raquillet <antoine.raquillet@invenietis.com>
2017-05-16 15:46:05 +0200

    Fixed hmr bug.
    Signed-off-by: Alex <apicard@intechinfo.fr>

--- DealCoin/DealCoin/DealCoin/dealcoin/build/dev-client.js
+++ DealCoin/DealCoin/DealCoin/dealcoin/build/dev-client.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 require('eventsource-polyfill')
-var hotClient = require('webpack-hot-middleware/client?noInfo=true&reload=true')
+var hotClient = require('webpack-hot-middleware/client?noInfo=true&reload=true&path=http://localhost:8080/__webpack_hmr')
 
 hotClient.subscribe(function (event) {
   if (event.action === 'reload') {
