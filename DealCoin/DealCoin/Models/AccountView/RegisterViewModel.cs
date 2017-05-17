1170bb3e3cf05b3455d44829c0b3496d6836eac7
Kamil <Kamil>
2017-05-17 15:13:57 +0200

    DAL+model+viewsaccount

---
+++ DealCoin/DealCoin/Models/AccountView/RegisterViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace DealCoin.Models.AccountView
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        //[StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Mot de passe")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmation mot de passe")]
+        [Compare("Password", ErrorMessage = "La confirmation du mot de passe ne correspond pas.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
