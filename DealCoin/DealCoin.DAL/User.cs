07156aca7dbeea0c7bbaf1b5a69054ac45004df0
Kamil <Kamil>
2017-06-06 09:52:02 +0200

    dal user

--- DealCoin/DealCoin.DAL/User.cs
+++ DealCoin/DealCoin.DAL/User.cs
@@ -14,16 +14,16 @@ namespace DealCoin.DAL
 
         public string GoogleId { get; set; }
 
-        public int Nom { get; set; }
+        public string Nom { get; set; }
 
-        public int Prenom { get; set; }
+        public string Prenom { get; set; }
 
-        public int Tel { get; set; }
+        public string Tel { get; set; }
 
-        public int Adresse { get; set; }
-        public int Departement { get; set; }
-        public int City { get; set; }
-        public int Postal { get; set; }
+        public string Adresse { get; set; }
+        public string Departement { get; set; }
+        public string City { get; set; }
+        public string Postal { get; set; }
         public string role { get; set; }
         public int status { get; set; }
         public int Visites { get; set; }
