2dda36c4c1b1daa1b1e89028bc5d58d654d4e112
Alex <apicard@intechinfo.fr>
2017-06-07 11:54:31 +0200

    article 3 avec chargement de catégories

---
+++ DealCoin/DealCoin.DAL/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DealCoin.DAL
+{
+    public class Category
+    {
+        public int categoriesId { get; set; }
+        public string name { get; set; }
+        public int parentId { get; set; }
+        public int type { get; set; }
+        public string description { get; set; }
+        public int status { get; set; }
+        public int position { get; set; }
+        public DateTime posted_date { get; set; }
+    }
+}
