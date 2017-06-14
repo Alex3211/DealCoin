ac90160711b35d6ac32e01243b00238a2179d977
Kamil <Kamil>
2017-06-14 10:32:34 +0200

    script categories

--- DealCoin/DealcoinDB/InsertArticle.sql
+++ DealCoin/DealcoinDB/InsertArticle.sql
@@ -4,10 +4,30 @@ GO
 USE [DealcoinDB]
 GO
 
-INSERT INTO [dc].[categories]([name],[parentId],[type],[description],[status],[position],[posted_date])VALUES('Téléphonie',0,2,'Téléphones',0,0,'2010-04-02')
-INSERT INTO [dc].[categories]([name],[parentId],[type],[description],[status],[position],[posted_date])VALUES('High-Tech et Informatique',0,2,'High-Tech et Informatique',0,0,'2010-04-02')
-INSERT INTO [dc].[categories]([name],[parentId],[type],[description],[status],[position],[posted_date])VALUES('Sports et Loisirs',0,2,'Sports et Loisirs',0,0,'2010-04-02')
-INSERT INTO [dc].[categories]([name],[parentId],[type],[description],[status],[position],[posted_date])VALUES('Vêtements, Chaussures, Bijoux',0,2,'Vêtements, Chaussures, Bijoux',0,0,'2010-04-02')
+INSERT INTO [dc].[categories]([name],[parentId],[type],[description],[status],[position],[posted_date])VALUES('Téléphonie',0,1,'Téléphones',0,0,'2010-04-02')
+INSERT INTO [dc].[categories]([name],[parentId],[type],[description],[status],[position],[posted_date])VALUES('High-Tech et Informatique',0,1,'High-Tech et Informatique',0,0,'2010-04-02')
+INSERT INTO [dc].[categories]([name],[parentId],[type],[description],[status],[position],[posted_date])VALUES('Sports et Loisirs',0,1,'Sports et Loisirs',0,0,'2010-04-02')
+INSERT INTO [dc].[categories]([name],[parentId],[type],[description],[status],[position],[posted_date])VALUES('Vêtements, Chaussures, Bijoux',0,1,'Vêtements, Chaussures, Bijoux',0,0,'2010-04-02')
+INSERT INTO [dc].[categories]([name],[parentId],[type],[description],[status],[position],[posted_date])VALUES('Téléphone portable',1,2,'Téléphone portable',0,0,'2010-04-02')
+INSERT INTO [dc].[categories]([name],[parentId],[type],[description],[status],[position],[posted_date])VALUES('Téléphone fixe',1,2,'Téléphone fixe',0,0,'2010-04-02')
+INSERT INTO [dc].[categories]([name],[parentId],[type],[description],[status],[position],[posted_date])VALUES('Housse et étuis',1,2,'Housse et étuis',0,0,'2010-04-02')
+INSERT INTO [dc].[categories]([name],[parentId],[type],[description],[status],[position],[posted_date])VALUES('Accessoires',1,2,'Accessoires',0,0,'2010-04-02')
+INSERT INTO [dc].[categories]([name],[parentId],[type],[description],[status],[position],[posted_date])VALUES('Appareil photo, Caméscopes',2,2,'Appareil photo, Caméscopes',0,0,'2010-04-02')
+INSERT INTO [dc].[categories]([name],[parentId],[type],[description],[status],[position],[posted_date])VALUES('TV, Home Cinéma',2,2,'TV, Home Cinéma',0,0,'2010-04-02')
+INSERT INTO [dc].[categories]([name],[parentId],[type],[description],[status],[position],[posted_date])VALUES('Audio, HiFi',2,2,'Audio, HiFi',0,0,'2010-04-02')
+INSERT INTO [dc].[categories]([name],[parentId],[type],[description],[status],[position],[posted_date])VALUES('Ordinateur',2,2,'Ordinateur',0,0,'2010-04-02')
+INSERT INTO [dc].[categories]([name],[parentId],[type],[description],[status],[position],[posted_date])VALUES('Fitness, Musculation',3,2,'Fitness, Musculation',0,0,'2010-04-02')
+INSERT INTO [dc].[categories]([name],[parentId],[type],[description],[status],[position],[posted_date])VALUES('Camping',3,2,'Camping',0,0,'2010-04-02')
+INSERT INTO [dc].[categories]([name],[parentId],[type],[description],[status],[position],[posted_date])VALUES('Football',3,2,'Football',0,0,'2010-04-02')
+INSERT INTO [dc].[categories]([name],[parentId],[type],[description],[status],[position],[posted_date])VALUES('Golf',3,2,'Golf',0,0,'2010-04-02')
+INSERT INTO [dc].[categories]([name],[parentId],[type],[description],[status],[position],[posted_date])VALUES('Vêtements de sport',3,2,'Vêtements de sport',0,0,'2010-04-02')
+INSERT INTO [dc].[categories]([name],[parentId],[type],[description],[status],[position],[posted_date])VALUES('Homme',4,2,'Homme',0,0,'2010-04-02')
+INSERT INTO [dc].[categories]([name],[parentId],[type],[description],[status],[position],[posted_date])VALUES('Femme',4,2,'Femme',0,0,'2010-04-02')
+INSERT INTO [dc].[categories]([name],[parentId],[type],[description],[status],[position],[posted_date])VALUES('Enfants',4,2,'Enfants',0,0,'2010-04-02')
+INSERT INTO [dc].[categories]([name],[parentId],[type],[description],[status],[position],[posted_date])VALUES('Bijoux',4,2,'Bijoux',0,0,'2010-04-02')
+INSERT INTO [dc].[categories]([name],[parentId],[type],[description],[status],[position],[posted_date])VALUES('Accessoires de mode',4,2,'Accessoires de mode',0,0,'2010-04-02')
+
+
 GO
 INSERT INTO [dc].[products]([userId],[categoriesId],[title],[photo],[desc1],[price],[visits],[posted_date],[created],[created_pk],[created_ip],[updated],[updated_pk],[updated_ip])VALUES(1,1,'Premier téléphone','./maphoto.png','Super 3310 !','10€',0,null,null,null,null,null,null,null)
 INSERT INTO [dc].[products]([userId],[categoriesId],[title],[photo],[desc1],[price],[visits],[posted_date],[created],[created_pk],[created_ip],[updated],[updated_pk],[updated_ip])VALUES(1,1,'Second téléphone','./maphoto.png','Super 3310 !','10€',0,null,null,null,null,null,null,null)
