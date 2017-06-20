2583fbaa617ef66962800212f6e7756d89719180
Victor <vandre@intechinfo.fr>
2017-06-20 12:46:39 +0200

    end of panier.vue

--- DealCoin/DealCoin/App/DealCoin/src/vue/Panier.vue
+++ DealCoin/DealCoin/App/DealCoin/src/vue/Panier.vue
@@ -6,28 +6,18 @@
                     <thead>
                         <tr>
                             <th>Product</th>
-                            <th>Quantity</th>
-                            <th class="text-center">Price</th>
                             <th class="text-center">Total</th>
                             <th> </th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
+                        <tr v-for="a of article" >
                             <td class="col-sm-8 col-md-6">
-                            <div class="media">
-                                <a class="thumbnail pull-left" href="#"> <img class="media-object" src="http://icons.iconarchive.com/icons/custom-icon-design/flatastic-2/72/product-icon.png" style="width: 72px; height: 72px;"> </a>
-                                <div class="media-body">
-                                    <h4 class="media-heading"><a href="#">Product name</a></h4>
-                                    <h5 class="media-heading"> by <a href="#">Brand name</a></h5>
-                                    <span>Status: </span><span class="text-success"><strong>In Stock</strong></span>
-                                </div>
-                            </div></td>
-                            <td class="col-sm-1 col-md-1" style="text-align: center">
-                            <input type="email" class="form-control" id="exampleInputEmail1" value="3">
+                            <a class="thumbnail pull-left"> <img class="media-object" src="http://icons.iconarchive.com/icons/custom-icon-design/flatastic-2/72/product-icon.png" style="width: 72px; height: 72px;"> </a>
+                                <h4>{{a.title}}</h4>
+                                <h5>{{a.desc1}}</h5>
                             </td>
-                            <td class="col-sm-1 col-md-1 text-center"><strong>$4.87</strong></td>
-                            <td class="col-sm-1 col-md-1 text-center"><strong>$14.61</strong></td>
+                            <td class="col-sm-1 col-md-1 text-center"><strong>{{a.price}}</strong></td>
                             <td class="col-sm-1 col-md-1">
                             <button type="button" class="btn btn-danger">
                                 <span class="glyphicon glyphicon-remove"></span> Remove
@@ -35,33 +25,24 @@
                         </tr>
                         <tr>
                             <td>   </td>
-                            <td>   </td>
-                            <td>   </td>
                             <td><h5>Total Hors Tax</h5></td>
-                            <td class="text-right"><h5><strong>$24.59</strong></h5></td>
+                            <td class="text-right"><h5><strong>{{totaleprix}} €</strong></h5></td>
                         </tr>
                         <tr>
-                            <td>   </td>
-                            <td>   </td>
                             <td>   </td>
                             <td><h5>Cout de livraison</h5></td>
-                            <td class="text-right"><h5><strong>$6.94</strong></h5></td>
+                            <td class="text-right"><h5><strong>{{livraison.toFixed(2)}} €</strong></h5></td>
                         </tr>
                         <tr>
-                            <td>   </td>
-                            <td>   </td>
                             <td>   </td>
                             <td><h3>Total</h3></td>
-                            <td class="text-right"><h3><strong>$31.53</strong></h3></td>
+                            <td class="text-right"><h3><strong>{{totaletopay.toFixed(2)}} €</strong></h3></td>
                         </tr>
                         <tr>
                             <td>   </td>
-                            <td>   </td>
-                            <td>   </td>
                             <td>
-                            <button type="button" class="btn btn-default">
-                                <span class="glyphicon glyphicon-shopping-cart"></span> Continue Shopping
-                            </button></td>
+                            <router-link to="/articles"><i class="fa fa-list-alt fa-fw"></i>Continue Shopping</router-link>
+                            </td>
                             <td>
                             <button type="button" class="btn btn-success">
                                 Checkout <span class="glyphicon glyphicon-play"></span>
@@ -81,7 +62,12 @@ import { mapGetters,mapActions } from 'vuex'
 export default {
         data() {
             return {
-                article:[]
+                article:[],
+                totaleprix:0,
+                tva:1.9,
+                livraison:0,
+                limitlivraison:49,
+                totaletopay:0
             }
         },
         async mounted() {
@@ -90,7 +76,21 @@ export default {
         methods: {
             ...mapGetters(['getArticle']),
             loadArticle: async function(){
-            this.article = this.getArticle();
+                this.article = this.getArticle();
+                
+                for(var i=0;i<this.article.length;i++){
+                this.totaleprix += parseInt(this.article[i].price)
+                }
+                if(this.totaleprix<=50){
+                    this.livraison = this.totaleprix/this.tva
+                }else{
+                    this.livraison =0
+                }
+                 
+                this.totaletopay = this.livraison+this.totaleprix
+            },
+            addprice(value){
+                this.totaleprix += value;
             }
         }
     }
